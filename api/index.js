01e8b4d305a6c19c7e09f25bd1e7239aec7c270f
EzequielGomez74 <ezequiel.canob@gmail.com>
2022-09-25 17:38:41 -0300

    index

--- api/index.js
+++ api/index.js
@@ -19,10 +19,16 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+//const axios = require('axios');
+//const dot = require('dotenv')
+
+//dot.config()
+//axios.defaults.baseURL = 'http://localhost:3001';
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
+
