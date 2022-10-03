8b20b41f3480332c3155e3293416ed1917e5413b
EzequielGomez74 <ezequiel.canob@gmail.com>
2022-10-03 15:24:36 -0300

    front pi

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -11,7 +11,7 @@ function About() {
         <div className="container-about">
         <div className="neon">
             <h1>PI HENRY VIDEOGAMES 2022</h1>
-            <h1>EZEQUIEL FERNANDO GOMEZ</h1>
+            <h1>EZEQUIEL GOMEZ</h1>
             </div>
              <div className="div-foto">
                <img src={imagen} alt="foto"></img>
