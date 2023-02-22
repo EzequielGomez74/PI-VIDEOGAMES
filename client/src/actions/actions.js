c18645b55004c025ecb279bf8db9886735037f9f
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-02-22 00:26:38 -0300

    deploy

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export function searchByName(name)  { // función que busca por nombre
   return function (dispatch) { // recibe el dispatch
     return axios
-    .get(`/videogames?name=${name}`) // hace un get a la ruta /videogames?name= + el nombre que se busca
+    .get(`https://pi-videogames-production-e98d.up.railway.app/videogames?name=${name}`) // hace un get a la ruta /videogames?name= + el nombre que se busca
       .then((res) => { // si la respuesta es exitosa, ejecuta la función dispatch con el tipo SEARCH_BY_NAME y los datos de la respuesta
         console.log(res)
         dispatch({ type: 'SEARCH_BY_NAME', payload: res.data }); // el payload es el array de juegos filtrados
@@ -19,7 +19,7 @@ export function searchByName(name)  { // función que busca por nombre
 export function getGenres() { // devuelve los géneros de la base de datos
   return async function (dispatch) { // devuelve una función asincrónica
     try {
-      var res = await axios.get('/genres'); // espera a que se resuelva la promesa
+      var res = await axios.get('https://pi-videogames-production-e98d.up.railway.app/genres'); // espera a que se resuelva la promesa
       console.log(res)
       return dispatch({  // devuelve el dispatch
         type: 'GET_GENRES', payload: res.data, // devuelve el tipo de acción y el payload
@@ -32,7 +32,7 @@ export function getGenres() { // devuelve los géneros de la base de datos
 export function getVideogameDetail(id) { // recibe el id del juego
   return function (dispatch) { // ejecuta la accion getVideogameDetail
     axios
-      .get(`/videogame/${id}`) // busca el juego por id
+      .get(`https://pi-videogames-production-e98d.up.railway.app/videogame/${id}`) // busca el juego por id
       .then((res) => { // si lo encuentra, lo guarda en el estado de gameDetails
         console.log(res)
         dispatch({ type: 'GET_VIDEOGAME_DETAIL', payload: res.data }); // guarda el juego en el estado de gameDetails
@@ -44,7 +44,7 @@ export function getVideogameDetail(id) { // recibe el id del juego
 }
 export function getAllGames() { // esta función se llama en el componente Videogames.js
   return function (dispatch) { 
-    return axios.get("/videogames/") // hace un get a la ruta /videogames
+    return axios.get("https://pi-videogames-production-e98d.up.railway.app/videogames/") // hace un get a la ruta /videogames
       .then((res) => { // si la respuesta es exitosa, ejecuta la función dispatch, que actualiza el estado de los juegos filtrados
         console.log(res)
         dispatch({ type: 'GET_ALL_GAMES', payload: res.data }); // el payload es la respuesta de la ruta /videogames
--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,9 @@ import axios from 'axios';
 // dotenv.config()
 
 
-axios.defaults.baseURL = "pi-videogames-production-e98d.up.railway.app" 
+// axios.defaults.baseURL = "pi-videogames-production-e98d.up.railway.app" 
 
-// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001" 
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001" 
 
 ReactDOM.render( // envuelve la app en el provider para que tenga acceso al store
   <React.StrictMode>
