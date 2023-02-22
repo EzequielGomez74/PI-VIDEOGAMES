2e2fee75d7c7ec854c52622ff8b5ec13de696100
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-02-22 00:13:53 -0300

    123

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,12 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import dotenv from 'dotenv';
 import axios from 'axios';
-dotenv.config()
+// dotenv.config()
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001" // si no hay una variable de entorno, se usa la local
+
+axios.defaults.baseURL = "pi-videogames-production-e98d.up.railway.app" 
+
+// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001" 
 
 ReactDOM.render( // envuelve la app en el provider para que tenga acceso al store
   <React.StrictMode>
